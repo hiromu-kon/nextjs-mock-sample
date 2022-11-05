4c3254c1bb7f327dd646bb0949cf5b93dcc1e811
hiromu-kon <hiro.0405soccer@icloud.com>
2022-11-05 21:31:47 +0900

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@ import '@/styles/globals.scss';
 import '@/styles/reset.scss';
 import type { AppProps } from 'next/app';
 
+if (process.env.NEXT_PUBLIC_API_MOCKING) {
+  require('../mocks');
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
